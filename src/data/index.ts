c1ea1bf941e24743d24eb48c3f50a811285611b9
Bacho0003 <103092030+Bacho0003@users.noreply.github.com>
2024-06-25 16:58:19 -0500

    Cambio de Precios correctos

--- src/data/index.ts
+++ src/data/index.ts
@@ -22,7 +22,7 @@ export const products: Product[] = [
     id: 32,
     name: "S-Crate Box",
     slug: "s-crate-box",
-    price: 18000,
+    price: 12000,
     link_whatsapp: 'https://wa.link/3n0fk2',
     description: "Esta box es increíble, tiene lo mínimo para asegurar la diversión con un grupo de panas. Procura llevar contigo un bote de agua ><",
     cover: "https://i.ibb.co/TtrhsLS/Whats-App-Image-2024-06-23-at-03-34-25-1.jpg",
@@ -34,7 +34,7 @@ export const products: Product[] = [
     id: 33,
     name: "M-Crate Box",
     slug: "m-crate-box",
-    price: 30000,
+    price: 18000,
     link_whatsapp: 'https://wa.link/3n0fk2',
     description: "Perfecta para una tarde inolvidable con amigos. La M-Crate Box incluye una selección equilibrada para asegurar una experiencia placentera y compartida. No olvides llevar tus snacks favoritos.",
     cover: "https://i.ibb.co/XYZ.png", // Actualiza la URL de la imagen de la caja mediana
@@ -46,7 +46,7 @@ export const products: Product[] = [
     id: 34,
     name: "XL-Crate Box",
     slug: "xl-crate-box",
-    price: 50000,
+    price: 100000,
     link_whatsapp: 'https://wa.link/3n0fk2',
     description: "La XL-Crate Box es ideal para grandes eventos o celebraciones. Con 50 unidades, esta caja garantiza que todos en la fiesta tengan una experiencia premium. ¡Asegúrate de tener suficiente espacio para compartir!",
     cover: "https://i.ibb.co/XYZ.png", // Actualiza la URL de la imagen de la caja extra grande
@@ -58,7 +58,7 @@ export const products: Product[] = [
     id: 35,
     name: "Max-Crate Box",
     slug: "max-crate-box",
-    price: 100000,
+    price: 200000,
     link_whatsapp: 'https://wa.link/3n0fk2',
     description: "Para los eventos más grandes, la Max-Crate Box es la opción definitiva. Con un máximo de 100 unidades, esta caja está diseñada para aquellos que buscan llevar su experiencia al siguiente nivel. Perfecta para festivales y grandes reuniones.",
     cover: "https://i.ibb.co/XYZ.png", // Actualiza la URL de la imagen de la caja máxima
