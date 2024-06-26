d081c932c41ef9924a090302bd0210a23f6af837
Bacho0003 <103092030+Bacho0003@users.noreply.github.com>
2024-06-26 03:17:44 -0500

    LOL

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,10 @@
 export const tutorialText = "About us";
 export const tutorialHref = "#";
 
-export const previousText = "Canal Youtube";
+export const previousText = "¡Obtenga mega descuentos al Whatsapp!";
 export const previousHref = "#";
 
-export const githubText   = "Contacto"
+export const githubText   = "otro"
 export const githubHref   = "#"
 
 export const titie = 'Crate Boxes'
