d46b019fe2b48c45af1283801fb8f3b57487c9af
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2024-06-26 01:44:34 -0400

    update imga

--- src/data/index.ts
+++ src/data/index.ts
@@ -25,7 +25,7 @@ export const products: Product[] = [
     price: 12000,
     link_whatsapp: 'https://wa.link/3n0fk2',
     description: "Esta box es increíble, tiene lo mínimo para asegurar la diversión con un grupo de panas. contiene 6 unidades. Procura llevar contigo un bote de agua ><",
-    cover: "https://cnnlxzoncmhijwubkbue.supabase.co/storage/v1/object/sign/crate%20boxes/s.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjcmF0ZSBib3hlcy9zLnBuZyIsImlhdCI6MTcxOTM3OTg3NSwiZXhwIjoyNjY1NDU5ODc1fQ.dmJ5GtWCEm9gb8g1P_SUbkq3QChN8rul0T4p2ArBjAE&t=2024-06-26T05%3A31%3A12.748Z",
+    cover: "/s.png",
     coverCredits: "",
     category: ProductCategory.cratebox,
     units: 6,
@@ -37,7 +37,7 @@ export const products: Product[] = [
     price: 18000,
     link_whatsapp: 'https://wa.link/3n0fk2',
     description: "Perfecta para una tarde inolvidable con amigos. La M-Crate Box incluye una selección equilibrada para asegurar una experiencia placentera y compartida. contiene 9 unidades,  No olvides llevar tus snacks favoritos.",
-    cover: "https://cnnlxzoncmhijwubkbue.supabase.co/storage/v1/object/sign/crate%20boxes/m.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjcmF0ZSBib3hlcy9tLmpwZyIsImlhdCI6MTcxOTM3OTk2NywiZXhwIjoyNjY1NDU5OTY3fQ.wQoZw9fmHTar0eLfQXbyxNLsnAYqjxRzbnCAedW3x0s&t=2024-06-26T05%3A32%3A45.563Z", // Actualiza la URL de la imagen de la caja mediana
+    cover: "m.jpg", // Actualiza la URL de la imagen de la caja mediana
     coverCredits: "",
     category: ProductCategory.cratebox,
     units: 9,
@@ -49,7 +49,7 @@ export const products: Product[] = [
     price: 100000,
     link_whatsapp: 'https://wa.link/3n0fk2',
     description: "La XL-Crate Box es ideal para grandes eventos o celebraciones. Con 50 unidades, esta caja garantiza que todos en la fiesta tengan una experiencia premium. ¡Asegúrate de tener suficiente espacio para compartir!",
-    cover: "https://cnnlxzoncmhijwubkbue.supabase.co/storage/v1/object/sign/crate%20boxes/m.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjcmF0ZSBib3hlcy9tLmpwZyIsImlhdCI6MTcxOTM3OTk2NywiZXhwIjoyNjY1NDU5OTY3fQ.wQoZw9fmHTar0eLfQXbyxNLsnAYqjxRzbnCAedW3x0s&t=2024-06-26T05%3A32%3A45.563Z", // Actualiza la URL de la imagen de la caja extra grande
+    cover: "xl.jpg", // Actualiza la URL de la imagen de la caja extra grande
     coverCredits: "",
     category: ProductCategory.cratebox,
     units: 50,
@@ -61,7 +61,7 @@ export const products: Product[] = [
     price: 200000,
     link_whatsapp: 'https://wa.link/3n0fk2',
     description: "Para los eventos más grandes, la Max-Crate Box es la opción definitiva. Con un máximo de 100 unidades, esta caja está diseñada para aquellos que buscan llevar su experiencia al siguiente nivel. Perfecta para festivales y grandes reuniones.",
-    cover: "https://cnnlxzoncmhijwubkbue.supabase.co/storage/v1/object/sign/crate%20boxes/extra.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjcmF0ZSBib3hlcy9leHRyYS5qcGciLCJpYXQiOjE3MTkzODAwMTIsImV4cCI6MjY2NTQ2MDAxMn0.X5NAklNjUyIwa0vQqRrHqb7XeQnizi2zvWBPLWo5qZs&t=2024-06-26T05%3A33%3A30.220Z", // Actualiza la URL de la imagen de la caja máxima
+    cover: "extra.jpg", // Actualiza la URL de la imagen de la caja máxima
     coverCredits: "",
     category: ProductCategory.cratebox,
     units: 100,
