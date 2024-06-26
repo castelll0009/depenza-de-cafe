4a6d4ee1956f8438686c9c508d7d256d24897de5
Bacho0003 <103092030+Bacho0003@users.noreply.github.com>
2024-06-26 05:30:50 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@
 export const tutorialText = "About us";
 export const tutorialHref = "#";
 
-export const previousText = "¡Obtenga mega descuentos al Whatsapp!";
+export const previousText = "¡Obtenga descuentos al retornar la caja!";
 export const previousHref = "#";
 
 export const githubText   = "otro"
